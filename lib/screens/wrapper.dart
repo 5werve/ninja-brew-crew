3c91e47c2c980ba30ac4d138f2a071708eeb4be0
Matthew Vu <vuminhmatthew@gmail.com>
2022-02-03 13:42:18 -0800

    Upload app

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:ninja_brew_crew/screens/home/authenticate/authenticate.dart';
+import 'package:ninja_brew_crew/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // return either Home or Authenticate widget
+    return Authenticate();
+  }
+}
